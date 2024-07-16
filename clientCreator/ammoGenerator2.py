c1882f1b327c8a2e9763d8b51a5f8856c1b473ef
Dmitrii Pyrkin <dmitrii.pyrkin@travelline.ru>
2024-07-16 16:25:06 +0300

    add ammoGenerator2

---
+++ clientCreator/ammoGenerator2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import asyncio
+import sys
+
+import aiohttp
+
+from tee import Tee
+
+
+import time
+import datetime
+
+from aiohttp import ClientSession, ClientError
+
+async def get_provider_list(session):
+    providers_url = 'http://bepool-qa.travelline.lan/api/rest/inventory/provider/list'
+    headers = {
+        'X-Inventory-ApiKey': '75010639-0955-4c90-aaaa-4c2b41544edb'
+    }
+    start_time = time.time()
+    async with session.get(providers_url, headers=headers, ssl=False) as response:
+        elapsed_time = time.time() - start_time
+        try:
+            response_data = await response.json()
+        except aiohttp.ContentTypeError:
+            response_data = None
+        return response.status, response_data, elapsed_time
+
+
+def make_ammo(method, url, headers, case, body):
+    """ makes phantom ammo """
+    # http request w/o entity body template
+    req_template = (
+        "%s %s HTTP/1.1\n"
+        "%s\n"
+        "\n"
+    )
+
+    # http request with entity body template
+    req_template_w_entity_body = (
+        "%s %s HTTP/1.1\r\n"
+        "%s\r\n"
+        "Content-Length: %d\r\n"
+        "\r\n"
+        "%s\r\n"
+    )
+
+    if not body:
+        req = req_template % (method, url, headers)
+    else:
+        req = req_template_w_entity_body % (method, url, headers, len(body), body)
+
+    # phantom ammo template
+    ammo_template = (
+        "%d %s\n"
+        "%s"
+    )
+
+    return ammo_template % (len(req) + 6, case, req)
+
+
+
+async def main():
+    client_owner_token = ""
+    async with aiohttp.ClientSession() as session:
+
+        provider_status, provider_data, provider_time = await get_provider_list(session)
+        if provider_status == 200:
+            provider_items = provider_data.get("Items", [])
+
+        providers = provider_items[:3]
+
+        sys.stdout.write("[Connection: close]\n")
+        sys.stdout.write("[Host: bookingreport-qa.travelline.lan:443]\n")
+
+        for provider in providers:
+            property_id = str(provider).strip()
+            url = f"/BookingReport/WebApi/PartnerApi/GetBookings?ProviderId={property_id}&LastModifiedMillisecondsFrom=1230757200000&LastModifiedMillisecondsTo=1262293200000&WithoutDate=True&limit=10\n"
+
+            sys.stdout.write(url)
+
+if __name__ == "__main__":
+    with open('../loadtest/ammo.txt', 'w') as f:
+        original_stdout = sys.stdout
+        sys.stdout = Tee(sys.stdout, f)
+        try:
+            asyncio.run(main())
+        finally:
+            sys.stdout = original_stdout
+
+
